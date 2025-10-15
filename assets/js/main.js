e972c34cfb59e1170a106885a91c560e3ce4dd36
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-15 21:41:02 +0200

    add ticket price calculation

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,37 @@ const ageEl = document.getElementById("age");
 const kmEl = document.getElementById("km");
 const btnEl = document.getElementById("btn");
 
+const sconto_minori = 0.2;
+const sconto_anziani = 0.4;
+const price = 0.21;
+
 btnEl.addEventListener("click", (event) => {
   event.preventDefault();
   const user_age = ageEl.value;
   const user_km = kmEl.value;
   console.log(user_age);
+
+  let final_price = 0;
+
+  if (user_age < 18) {
+    //calcolo prezzo per mocciosi
+    final_price = (price - price * sconto_minori).toFixed(2);
+    console.log(final_price);
+  } else if (ages > 65) {
+    //calcolo prezzo per nonnetti
+    final_price = (price - price * sconto_anziani).toFixed(2);
+    console.log(final_price);
+  } else {
+    //prezzo pieno
+    final_price = price;
+    final_price = final_price.toFixed(2);
+    console.log(final_price);
+  }
+  //aggiungo tutto il calcolo relativo
 });
 
 /*
 MILESTONE 2:
-Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. 
+realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. 
 Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 */
