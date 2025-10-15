f8c3e18dca515abe6c9781a753289ffac1179a06
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-15 21:21:04 +0200

    FUNZIONAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,10 @@ console.log("prova");
 const ageEl = document.getElementById("age");
 const kmEl = document.getElementById("km");
 const btnEl = document.getElementById("btn");
+
+btnEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  const user_age = ageEl.value;
+  const user_km = kmEl.value;
+  console.log(user_age);
+});
