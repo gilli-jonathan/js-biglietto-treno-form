08fe8d4033f578aba5eb72097f9d5e89f93ba132
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-15 21:23:32 +0200

    Starting second milestone

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,9 @@ btnEl.addEventListener("click", (event) => {
   const user_km = kmEl.value;
   console.log(user_age);
 });
+
+/*
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. 
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+*/
